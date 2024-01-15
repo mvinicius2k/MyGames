2975e6a932466bf6599e81489d5b8ed137a46222
mvinicius2k <marcos.vinicius124816@gmail.com>
2024-01-15 15:04:45 -0300

    Forecasts teste corrigido

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,14 +2,29 @@ using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp 
     => new HttpClient 
     { 
-        BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? throw new Exception("Api não encontrada")) 
+        BaseAddress = GetApiUri(builder)
     });
 
 await builder.Build().RunAsync();
+
+
+
+
+
+static Uri GetApiUri(WebAssemblyHostBuilder builder){
+    #if DEBUG
+            return new Uri(builder.Configuration["API_Prefix"] ?? throw new Exception("Api não encontrada"));
+        #else
+            return new Uri(builder.HostEnvironment.BaseAddress);
+        #endif
+}
